2eb0985141e142d4f3f5349d8f822fefc9d3a796
Mohemmed Ibrahim M <mohemmedibrahimm23it@psnacet.edu.in>
2026-07-27 18:40:32 +0530

    Add files via upload

---
+++ Deep Skilling/Week 2/Spring Core_maven/Ex 7 Implementing Constructor and Setter/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,29 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Constructor Injection Executed");
+    }
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Setter Injection Executed");
+    }
+
+    public void addBook() {
+
+        System.out.println("Book Service Started");
+
+        bookRepository.saveBook();
+
+    }
+
+}
