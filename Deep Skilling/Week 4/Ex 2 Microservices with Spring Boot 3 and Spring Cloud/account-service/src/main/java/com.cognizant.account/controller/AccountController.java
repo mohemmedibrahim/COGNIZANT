4beab9de36fda88fca06bf094bfe29d175d3e8be
Mohemmed Ibrahim M <mohemmedibrahimm23it@psnacet.edu.in>
2026-07-27 18:33:57 +0530

    Add files via upload

---
+++ Deep Skilling/Week 4/Ex 2 Microservices with Spring Boot 3 and Spring Cloud/account-service/src/main/java/com.cognizant.account/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/account")
+    public String getAccount() {
+
+        return "Account Service is Running";
+
+    }
+
+}
---
+++ Deep Skilling/Week 4/Ex 2 Microservices with Spring Boot 3 and Spring Cloud/loan-service/src/main/java/com.cognizant.loan/controller/LoanController.java
@@ -0,0 +1,16 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loan")
+    public String getLoan() {
+
+        return "Loan Service is Running";
+
+    }
+
+}
